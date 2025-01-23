691a2f675d168d717a864fe0489f19af2868c530
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-01-23 21:02:58 +0000

    Updated doc

--- src/SampleClass.cpp
+++ src/SampleClass.cpp
@@ -2,16 +2,15 @@
 #include "../include/SampleClass.h"
 
 /*******************************************************************************
- Function:    factorial
+Function:     factorial
 
- Description: The factorial of a number is defined using the following formula
-              n! = n * (n - 1) * (n - 2) * ... * 1
-              The factorial of 0 is defined to be 1.
-              The factorial of a negative number is undefined and will return -1
+Description:  The factorial of a number is defined using the folloiwng formula
+              n! = n * (n - 1) * (n - 2) * ... * 2 * 1
+              The factorial of 0 is defined to be 1
+              
+Parameter:    uInt - the number to calculate the factorial of
 
- Parameters:  anInt - The number to calculate the factorial of
-
- Returned:    The factorial of the number
+Returned      The factorial of the parameter
 *******************************************************************************/
 unsigned int SampleClass::factorial (unsigned int uInt) {
   unsigned int result = 1;
